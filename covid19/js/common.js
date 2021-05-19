f5b8a615b6ed7ad26939c6579c2cfa152507c24b
LucasChien <lucas.chien@eyesmedia.com.tw>
2021-05-19 13:45:58 +0800

    test covid19-20

--- covid19/js/common.js
+++ covid19/js/common.js
@@ -11,3 +11,37 @@ function parseURLparm() {
   }
   return vars;
 }
+
+
+// 區分要打開哪一個 endpointType 的div區塊
+function showEndpointTypeBlock (endpointType) {
+  // 消費者端
+  if (endpointType === 'REISTER') { // 已申請通行證,未申請通行證
+    $('.endpointType_REISTER').addClass('show');
+    $('.main').addClass('recording');
+  }
+  if (endpointType === 'FOOTPRINT') { // 28日足跡
+    $('.endpointType_FOOTPRINT').addClass('show');
+    $('.main').addClass('footprint');
+  }
+
+  // 店家端
+  if (endpointType === 'STANDARD') { // 一般類
+    $('.endpointType_STANDARD').addClass('show');
+    $('.main').addClass('normal');
+  }
+  if (endpointType === 'TRAFFIC') { // 交通類
+    $('.endpointType_TRAFFIC').addClass('show');
+    $('.main').addClass('traffic');
+  }
+  if (endpointType === 'B_FOOTPRINT') { // 28日足跡
+    $('.endpointType_B_FOOTPRINT').addClass('show');
+    $('.main').addClass('b-footprint');
+  }
+  if (endpointType === 'QRCODE') { // 下載QA code
+    $('.endpointType_QRCODE').addClass('show');
+    $('.main').addClass('qrcode');
+  }
+
+
+}
