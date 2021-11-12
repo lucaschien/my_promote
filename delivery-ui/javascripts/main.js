4f4c3779ea947f2fea8957816aa819ca1ee2885a
lucaschien <lucas.chien@eyesmedia.com.tw>
2021-11-12 21:37:48 +0800

    test 運力

--- delivery-ui/javascripts/main.js
+++ delivery-ui/javascripts/main.js
@@ -1,9 +1,9 @@
 $(function() {
   // 解決桌機,手機css height 100vh的問題
   let windowsVH = window.innerHeight / 100;
-  document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+  document.querySelector('.main').style.setProperty('--vh', windowsVH + 'px');
   window.addEventListener('resize', function() {
-    document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+    document.querySelector('.main').style.setProperty('--vh', windowsVH + 'px');
   });
 
   // 通用事件 - 所有高版的 dialog 都可以用此關閉 dialog
