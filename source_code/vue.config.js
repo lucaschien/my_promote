68da24ae747d5ac89aeaecb7a6831e57774c884f
Lucas Chien <bert507@gmail.com>
2022-03-05 23:29:47 +0800

    增加打包環境變數

--- source_code/vue.config.js
+++ source_code/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'my_promote',
+  publicPath: process.env.VUE_APP_routerPATH,
+  devServer: {
+    port: 8080,
+    open: true
+  }
 })
